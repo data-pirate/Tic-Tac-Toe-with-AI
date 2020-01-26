e991ab7fb311cea9c1e0c5660156eff19a45c80b
Damanpreet Singh <43677641+data-pirate@users.noreply.github.com>
2020-01-26 10:36:51 +0530

    update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,11 +1,12 @@
 # Tic Tac Toe game with Python 
+import random
 
 board = [' ' for i in range(10)] 
 
 def insertLetter(letter, pos):
     board[pos] = letter
 
-def printBoard():
+def printBoard(board):
     print('   ||   ||')
     print(' ' + board[1] + ' || ' + board[2] + ' || ' + board[3])
     print('   ||   ||')
@@ -18,7 +19,7 @@ def printBoard():
     print(' ' + board[7] + ' || ' + board[8] + ' || ' + board[9])
     print('   ||   ||')
 
-def isWinner(board_pos, letter):
+def isWinner(board, letter):
     return( board[1] == letter and board[2] == letter and board[3] == letter or
         board[4] == letter and board[5] == letter and board[6] == letter or
         board[7] == letter and board[8] == letter and board[9] == letter or
@@ -39,38 +40,93 @@ def playerMove():
         letter = input(' Enter Position to insert your \'X\' : ') 
         try:
             letter = int(letter)
-            if 0 > letter > 9:
+            if 0 < letter < 10:
+                if spaceisFree(letter):
+                    flag = False
+                    insertLetter('X', letter)
+                else:
+                    print('That space is occupied !!')
+            else:
                 print('Please enter a valid position !!!')
-            if spaceisFree(letter):
-                insertLetter('X', letter)
+
         except:
-            pass
-        break
+            print(' Type a number You enterd:',letter)
 
 def isBoardfull(board):
-    if board.count(' ') > 1:
-        return True
-    else:
-        return False
+    return board.count(' ') < 1
+
+def selectRandom(lst):
+    
+    return lst[random.randrange(1,len(lst))]
+
+
+def compMove():
+    move = 0
+
+# collecting all the possible moves
+
+    available_places = [i for i, letter in enumerate(board) if letter == ' ' and i != 0]
+    
+    for each in ['X', 'O']:
+        for i in available_places:
+            cpy = list(board)
+            cpy[i] = each #Checking if the player will win using next move , The computer will try to block that move
+            if isWinner(cpy, each):
+                move = i
+                return move
+
+# checking if the corners positions are available 
+
+    corners = list()
+    for i in available_places:
+        if i in [1,3,7,9]:
+            corners.append(i)
+    if len(corners) > 0:
+        move = selectRandom(corners)
+        return move
+
+# checking if the center is available
+
+    if 5 in available_places:
+        move = 5
+        return move
+
+# check if position 2, 4, 6 ,8 are available
+
+    center_pos = list()
+    for i in available_places:
+        if i in [2,4,6,8]:
+            center_pos.append(i)
+    if len(center_pos) > 0:
+        move = selectRandom(center_pos)
+        
+    return move
+
+    
 
 def main():
     print('\n<====  Welcome to the Tic Tac Toe  ====>\n')
-    printBoard()
+    printBoard(board)
 
     while not(isBoardfull(board)):
         if not(isWinner(board, 'O')):
             playerMove()
-            printBoard()
-            break
+            printBoard(board)
         else:
             print("Sorry, \'O\' wins the game !")
             break
 
         if not(isWinner(board, 'X')):
-            playerMove()
-            printBoard()
+            
+            move = compMove()
+            if move == 0:
+                print('Game Tied !!!!!')
+            else:
+                insertLetter('O', move)
+                print('Computer placed an \'O\' at position {}'.format(move))
+                printBoard(board)
         else:
-            print("Sorry, \'X\' wins the game !")
+            print("Hurray !, \'X\' wins the game !")
             break
 
     if isBoardfull(board):
